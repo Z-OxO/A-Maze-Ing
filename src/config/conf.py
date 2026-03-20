b6882ba7c5f379d8fd61b9161ce89fa25aaef4db
Nicolas Linotte <nilinott@z2r2p8.42lyon.fr>
2026-03-20 18:12:18 +0100

    remove read_file.py and add parsing function in conf.py

--- src/config/conf.py
+++ src/config/conf.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Conf():
     def __init__(self,
                  width: int, height: int,
@@ -32,3 +35,47 @@ class Conf():
 
     def get_perfect(self) -> bool:
         return (self.__perfect)
+
+
+def get_value(key) -> str | None:
+    try:
+        with open(sys.argv[1], "r") as file:
+            for line in file:
+                line = line.strip()
+
+                if (line.startswith(key)):
+                    parts = line.split("=")
+                    if (len(parts) == 2):
+                        return (parts[1].strip())
+    except FileNotFoundError:
+        pass
+    return (None)
+
+
+def parsing() -> None:
+    if (len(sys.argv) < 2):
+        print("Error !")
+        return
+    try:
+        width: int = int(get_value("WIDTH"))
+        height: int = int(get_value("HEIGHT"))
+        output_file: str = str(get_value("OUTPUT_FILE"))
+        entry: tuple[int, int] = (get_value("ENTRY"))
+        entry.split(',')
+        exit_maze: tuple[int, int] = get_value("EXIT")
+        exit_maze.split(',')
+        perfect: bool = (True if get_value("PERFECT") == "True" else
+                         (False if get_value("PERFECT") == "False" else None))
+        if (perfect is None):
+            print("Error : perfect key !")
+            return (None)
+        value: Conf = Conf(width, height, output_file, entry, exit_maze,
+                           perfect)
+        print(value.get_entry())
+    except (TypeError, ValueError):
+        print("Error: Key not found or value error !")
+        return
+
+
+if __name__ == "__main__":
+    parsing()
